2475f64cb15c0460ee1e32f711121c66e152e062
Andrew Niemantsverdriet <andrewNiemants@gmail.com>
2022-12-06 11:30:26 -0700

    use a blade component

--- resources/views/turnstileWidget.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="cf-turnstile"
-     data-sitekey="{{ config('services.turnstile.key') }}"
-     @foreach($configurations ?? [] as $key => $value) data-{{ $key }}="{{ $value }}" @endforeach
-></div>
