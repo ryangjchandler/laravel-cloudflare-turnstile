296f62ea617719af9bc57f1efd012ce21aa254dc
Andrew Niemantsverdriet <andrewNiemants@gmail.com>
2022-12-07 13:49:13 -0700

    wip

--- resources/views/components/render.blade.php
+++ resources/views/components/render.blade.php
@@ -2,8 +2,13 @@
      data-sitekey="{{ config('services.turnstile.key') }}"
      @if($attributes->has('wire:model'))
          {{ $attributes->filter(fn ($value, $key) => ! in_array($key, ['data-callback', 'wire:model'])) }}
-         data-callback="function(token) { $wire.set({{ $attributes->get('wire:model') }}, token); }"
+         data-callback="captchaCallback"
      @else
          {{ $attributes->whereStartsWith('data-') }}
      @endif
 ></div>
+<script>
+function captchaCallback(token) {
+    console.log(token);
+}
+</script>
