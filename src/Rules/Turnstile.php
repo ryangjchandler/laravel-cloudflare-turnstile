2be8e2e04d069dbc318f9f25bb90e9f886f823f9
Ryan Chandler <ryangjchandler@gmail.com>
2025-02-26 19:42:22 +0000

    Add turnstile custom rule

--- src/Rules/Turnstile.php
+++ src/Rules/Turnstile.php
@@ -23,7 +23,16 @@ class Turnstile implements Rule
         }
 
         foreach ($response->errorCodes as $errorCode) {
-            $this->messages[] = $this->mapErrorCodeToMessage($errorCode);
+            $this->messages[] = match ($errorCode) {
+                'missing-input-secret' => 'The secret parameter was not passed.',
+                'invalid-input-secret' => 'The secret parameter was invalid or did not exist.',
+                'missing-input-response' => 'The response parameter was not passed.',
+                'invalid-input-response' => 'The response parameter is invalid or has expired.',
+                'bad-request' => 'The request was rejected because it was malformed.',
+                'timeout-or-duplicate' => 'The response parameter has already been validated before.',
+                'internal-error' => 'An internal error happened while validating the response.',
+                default => 'An unexpected error occurred.',
+            };
         }
 
         return false;
@@ -32,19 +41,5 @@ class Turnstile implements Rule
     public function message()
     {
         return $this->messages;
-    }
-
-    protected function mapErrorCodeToMessage(string $code): string
-    {
-        return match ($code) {
-            'missing-input-secret' => 'The secret parameter was not passed.',
-            'invalid-input-secret' => 'The secret parameter was invalid or did not exist.',
-            'missing-input-response' => 'The response parameter was not passed.',
-            'invalid-input-response' => 'The response parameter is invalid or has expired.',
-            'bad-request' => 'The request was rejected because it was malformed.',
-            'timeout-or-duplicate' => 'The response parameter has already been validated before.',
-            'internal-error' => 'An internal error happened while validating the response.',
-            default => 'An unexpected error occurred.',
-        };
     }
 }
