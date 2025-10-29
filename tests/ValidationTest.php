a8c117d99615373075685f1b4a5de9b8db823e80
Ryan Chandler <ryangjchandler@gmail.com>
2025-10-29 14:05:58 -0400

    Add test for token failures

--- tests/ValidationTest.php
+++ tests/ValidationTest.php
@@ -18,3 +18,18 @@ it('can use the rule object for validation', function () {
 
     expect($validator->passes())->toBeTrue();
 });
+
+it('fails validation with an invalid token', function () {
+    config()->set('services.turnstile', [
+        'key' => Client::SITEKEY_ALWAYS_PASSES_VISIBLE,
+        'secret' => Client::SECRET_KEY_ALWAYS_FAILS,
+    ]);
+
+    $validator = Validator::make([
+        'cf-turnstile-response' => Client::RESPONSE_DUMMY_TOKEN,
+    ], [
+        'cf-turnstile-response' => [app(Turnstile::class)],
+    ]);
+
+    expect($validator->fails())->toBeTrue();
+});
